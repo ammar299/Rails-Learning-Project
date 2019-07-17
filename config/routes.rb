410e6cd1231d5acc53643e5c62a8bce30a52fa37
ammar299 <ammynice0@gmail.com>
2019-07-18 04:46:22 +0500

    Curd+webview

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
-Rails.application.routes.draw do
-	 root 'static_pages#home'
-	 get '/press', to: 'static_pages#press'
-	 get '/info', to: 'static_pages#info'
+#Rails.application.routes.draw do
+ #    get 'welcome/index'
+	# root 'static_pages#home'
+	# get '/press', to: 'static_pages#press'
+	# get '/info', to: 'static_pages#info'
+#	 get '/articles(.:format)' , to: 'articles#index'
+#	 get '/articles/new(.:format)', to: 'articles#new'
+#	 get '/articles/:id/edit(.:format) ' , to: 'articles#edit'
+ #    root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#end
+Rails.application.routes.draw do
+  get 'welcome/index'
+ 
+  resources :articles
+ 
+  root 'welcome#index'
 end
